867e8b7a48df3a56c882c42041309f68ad97034a
YORYI OJEDA <yoryiojeda@outlook.com>
2019-12-30 18:57:43 -0500

    web with home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//componentes
+import Inicio from'./components/inicio';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hola mundo hermoso
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+  
+<Inicio/>
+
+);
 }
 
 export default App;
